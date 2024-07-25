60015f7d1cff23dcf00237fcc7ddd842552c7837
Mahadevan Reji <rmahadevan574@gmail.com>
2024-07-25 22:48:13 +0530

    hmm

--- src/componets/pitchForm.jsx
+++ src/componets/pitchForm.jsx
@@ -4,7 +4,7 @@ function PitchForm(props) {
   const [pitch, setPitch] = useState("");
   return (
     <>
-      <h1>Pitch an idea!!!</h1>
+      <h1>Pitch an idea!!!!</h1>
       <textarea
         placeholder="Tell me about your idea and what problem it fixes...."
         value={pitch}
